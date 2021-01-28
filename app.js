2265203a682f2e3d9235310a775474a5c48085e9
ranjeet2003 <ranjeetgautam13032@gmail.com>
2021-01-28 18:02:59 +0530

    making a post request

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from express server');
+  res.status(200).json({
+    message: 'Hello from express',
+    app: 'Natours',
+  });
+});
+
+app.post('/', (req, res) => {
+  res.send('You are making a post request!...');
 });
 
 const port = 3000;
