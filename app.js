5e66538b6ce902f8c071324639be9bc2cabbfba2
ranjeet2003 <ranjeetgautam13032@gmail.com>
2021-03-09 12:21:12 +0530

    After module 6 working

--- app.js
+++ app.js
@@ -28,4 +28,9 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is hosting on port ${port}...👍`);
+});
+
 module.exports = app;
